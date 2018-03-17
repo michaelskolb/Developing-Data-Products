af9afcb35ae59216ee26f18f83eac1f1119f926b
michaelskolb <32855300+michaelskolb@users.noreply.github.com>
2018-03-17 16:56:23 -0700

    Add files via upload

--- IrisSlides.md
+++ IrisSlides.md
@@ -40,6 +40,22 @@ The Shiny user interface
 
 R code implements each slider bar and Predict Species button
 
+
+```r
+library(shiny)
+    sidebarPanel(
+      sliderInput("s1", label = strong("Sepal Length"), value=5, min=4.3, 
+                  max=7.9, step = 0.1),
+      sliderInput("s2", label = strong("Sepal Width"), value=3, min=2, 
+                  max=4.4, step = 0.1),
+      sliderInput("p1", label = strong("Petal Length"), value=3, 
+                  min=1, max=6.9, step = 0.1),
+      sliderInput("p2", label = strong("Petal Width"), value=2, min=0.1, 
+                  max=2.5, step = 0.1),
+      submitButton("Predict Species")
+    )
+```
+
 <!--html_preserve--><div class="col-sm-4">
 <form class="well">
 <div class="form-group shiny-input-container">
--- ui.R
+++ ui.R
@@ -2,14 +2,13 @@ library(shiny)
 library(datasets)
 data(iris)
 
-# Define UI for application that draws a histogram
 shinyUI(pageWithSidebar(
   
   # Application title
   headerPanel("Iris Species Predictor"),
   # Sidebar with a slider input for features 
     sidebarPanel(
-      p('Slide bars below to input Sepal and Petal length and width
+      p('Slide the bars below to input Sepal and Petal length and width
         measurements:'),
       
       sliderInput("s1", label = strong("Sepal Length"), value=5, min=4.3, 
@@ -19,14 +18,15 @@ shinyUI(pageWithSidebar(
       sliderInput("p1", label = strong("Petal Length"), value=3, 
                   min=1, max=6.9, step = 0.1),
       sliderInput("p2", label = strong("Petal Width"), value=2, min=0.1, 
-                  max=2.5, step = 0.1),
-      
-      submitButton("Predict Species")
+                  max=2.5, step = 0.1)
     ),
     
     mainPanel(
+      p('When Sepal and Petal length and width measurements are input,
+        click the button below:'),
+      submitButton("Predict Species"),
       
-      h1('Iris Species:'),
+      h3('Iris Species:'),
       verbatimTextOutput("pred")
     )
 ))
